6ee61f98a40b3da2ed25dbdd3fd2e4d7d6e6afa5
Agustín Gugliuzza <98058337+agusgugliu@users.noreply.github.com>
2024-01-20 22:52:32 +0100

    change script to add attempts

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 let randomNumber = Math.floor(Math.random() * 100) + 1;
+let attempts = 5;
 
 function checkGuess() {
-  const inputElement = document.getElementById("guess");
-  const feedbackElement = document.getElementById("feedback");
-  const guess = inputElement.value;
-  if (guess == randomNumber) {
-    feedbackElement.innerHTML = "Congratulations!";
-    feedbackElement.style.color = "green";
-  } else if (guess < randomNumber) {
-    feedbackElement.innerHTML = "Too low! Try again.";
-    feedbackElement.style.color = "red";
-  } else {
-    feedbackElement.innerHTML = "Too high! Try again.";
-    feedbackElement.style.color = "red";
-  }
+    while (attempts > 0) {
+        const inputElement = document.getElementById("guess");
+        const feedbackElement = document.getElementById("feedback");
+        const guess = inputElement.value;
+        if (guess == randomNumber) {
+            feedbackElement.innerHTML = "Congratulations!";
+            feedbackElement.style.color = "green";
+            break;
+        } else if (guess < randomNumber) {
+            feedbackElement.innerHTML = "Too low! Try again.";
+            feedbackElement.style.color = "red";
+            break;
+            attempts--;
+        } else {
+            feedbackElement.innerHTML = "Too high! Try again.";
+            feedbackElement.style.color = "red";
+            break;
+            attempts--;
+        }
+    }
+
+    if (attempts === 0) {
+        feedbackElement.innerHTML = "GAME OVER";
+    }
 }
