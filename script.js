c38b51f1f43d3d6b04aa298e2c9281818cb15c10
Agustín Gugliuzza <98058337+agusgugliu@users.noreply.github.com>
2024-01-21 07:55:35 +0100

    set GAME OVER message when there are no more attempts remaining

--- script.js
+++ script.js
@@ -2,28 +2,29 @@ let randomNumber = Math.floor(Math.random() * 100) + 1;
 let attempts = 5;
 
 function checkGuess() {
-    while (attempts > 0) {
-        const inputElement = document.getElementById("guess");
-        const feedbackElement = document.getElementById("feedback");
-        const guess = inputElement.value;
-        if (guess == randomNumber) {
-            feedbackElement.innerHTML = "Congratulations!";
-            feedbackElement.style.color = "green";
-            break;
-        } else if (guess < randomNumber) {
-            attempts--;
-            feedbackElement.innerHTML = "TOO LOW... Attempts Remaining: " + attempts;
-            feedbackElement.style.color = "red";
-            break;
+    while (attempts >= 0) {
+        if (attempts === 0) {
+            feedbackElement.innerHTML = "GAME OVER";
         } else {
-            attempts--;
-            feedbackElement.innerHTML = "TOO HIGH... Attempts Remaining: " + attempts;
-            feedbackElement.style.color = "red";
-            break;
+            const inputElement = document.getElementById("guess");
+            const feedbackElement = document.getElementById("feedback");
+            const guess = inputElement.value;
+            if (guess == randomNumber) {
+                feedbackElement.innerHTML = "Congratulations!";
+                feedbackElement.style.color = "green";
+                break;
+            } else if (guess < randomNumber) {
+                attempts--;
+                feedbackElement.innerHTML = "TOO LOW... Attempts Remaining: " + attempts;
+                feedbackElement.style.color = "red";
+                break;
+            } else {
+                attempts--;
+                feedbackElement.innerHTML = "TOO HIGH... Attempts Remaining: " + attempts;
+                feedbackElement.style.color = "red";
+                break;
+            }
         }
     }
-
-    if (attempts === 0) {
-        feedbackElement.innerHTML = "GAME OVER";
-    }
+    
 }
