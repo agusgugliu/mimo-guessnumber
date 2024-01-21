0d14be7653675a6cd737e29a1c696a499f630922
Agustín Gugliuzza <98058337+agusgugliu@users.noreply.github.com>
2024-01-21 08:49:08 +0100

    styled message display

--- script.js
+++ script.js
@@ -2,25 +2,41 @@ let randomNumber = Math.floor(Math.random() * 100) + 1;
 let attempts = 5;
 
 function checkGuess() {
-    while (attempts >= 0) {
+    while (attempts > 0) {
         const inputElement = document.getElementById("guess");
         const feedbackElement = document.getElementById("feedback");
+        const feedbackMessage = document.getElementById("fdbMessage");
+        const feedbackAttempts = document.getElementById("fdbAttempts");
         const guess = inputElement.value;
         
         if (guess == randomNumber) {
-            feedbackElement.innerHTML = "CONGRATULATIONS! The number was\t" + randomNumber;
-            feedbackElement.style.color = "green";
+            
+            feedbackMessage.innerHTML = "CONGRATS!";
+            feedbackMessage.style.color = "#0E2246";
             break;
+
         } else if (guess < randomNumber) {
+            
             attempts--;
-            feedbackElement.innerHTML = "TOO LOW!\nAttempts Remaining:\t" + attempts;
-            feedbackElement.style.color = "red";
+            
+            feedbackMessage.innerHTML = "TOO LOW.";
+            feedbackMessage.style.color = "#A84A54";
+
+            feedbackAttempts.innerHTML = "Attempts Remaining: " + attempts;
+            
             break;
+
         } else {
+            
             attempts--;
-            feedbackElement.innerHTML = "TOO HIGH!\nAttempts Remaining:\t" + attempts;
-            feedbackElement.style.color = "orange";
+            
+            feedbackMessage.innerHTML = "TOO HIGH.";
+            feedbackMessage.style.color = "#A84A54";
+
+            feedbackAttempts.innerHTML = "Attempts Remaining: " + attempts;
+
             break;
+
         }
     }
 }
